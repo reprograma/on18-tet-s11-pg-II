72d0e6528103bb046d3e58735793af9e47284b7d
GAGABY03 <gabrielamontarroyos3216@gmail.com>
2022-10-26 20:55:22 -0300

    Resolução semana 11.

--- exercicios/para-sala/src/controller/alunaController.js
+++ exercicios/para-sala/src/controller/alunaController.js
@@ -93,8 +93,47 @@ const obterNotas = async (req, res) => {
 
 const obterBoletim = async (req, res) => {
    // para casa
-}
-
+   
+    const { turma } = req.params
+    try {
+      const alunas = await db()
+      const alunasEncontradas = alunas.filter(alunaAtual => alunaAtual.turma == turma)
+      if (alunasEncontradas.length == 0) {
+        return res.status(404).json({ message: `Nenhuma aluna encontrada para a turma de ${turma}.` })
+      }
+      let notasAlunas;
+      let media;
+      let alunasFiltradas = [];
+      alunasEncontradas.forEach(aluna => {
+        notasAlunas = Object.values(aluna.notas)
+        media = (notasAlunas.reduce((acumulador, nota) => Number(acumulador) + Number(nota))) / 5
+        if (media >= 6) {
+          situacao = "APROVADA"
+        } else if (media >= 5) {
+          situacao = "RECUPERAÇÃO"
+        } else {
+          situacao = "REPROVADA"
+        }
+        const resposta = {
+          ciencias_da_natureza: aluna.notas.ciencias_da_natureza,
+          ciencias_humanas: aluna.notas.ciencias_humanas,
+          linguagens_codigos: aluna.notas.linguagens_codigos,
+          matematica: aluna.notas.matematica,
+          redacao: aluna.notas.redacao,
+          situacao: situacao,
+          media: Number(media.toFixed(2)),
+          nome: aluna.nome_social || aluna.nome_registro,
+          turma: aluna.turma
+        }
+        alunasFiltradas.push(resposta)
+      });
+  
+      res.status(200).send(alunasFiltradas)
+  
+    } catch (error) {
+      res.status(500).send({ message: error.message })
+    }
+  }
 const criarAluna = async (req, res) => {
    try {
      const alunas = await db()
