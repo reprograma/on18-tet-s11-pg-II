5eacc7efbbdb35b5071925ad99355d944b58f8f7
MBTGrazielle <grazimelo72@gmail.com>
2022-10-27 10:00:32 -0300

    Resolvi conflito, enviando novamente

--- exercicios/para-sala/src/controller/alunaController.js
+++ exercicios/para-sala/src/controller/alunaController.js
@@ -3,71 +3,71 @@ const db = require("../database/db")
 const crypto = require("crypto")
 
 const obterTodasAsAlunas = async (req, res) => {
-   try {
-     const alunas = await db()
+  try {
+    const alunas = await db()
 
-     const { nome, cidade, bairro, pai, mae } = req.query 
+    const { nome, cidade, bairro, pai, mae } = req.query
 
-     let alunasFiltradas = alunas.slice()
-  
-     if (nome) {
-        alunasFiltradas = alunasFiltradas.filter(alunaAtual => {
-            const nome_registro = alunaAtual.nome_registro.toLowerCase()
-            
-            let nome_social = alunaAtual.nome_social
+    let alunasFiltradas = alunas.slice()
 
-            if (nome_social)  { 
-              nome_social = nome_social.toLowerCase()
-              return nome_social.includes(nome) || nome_registro.includes(nome)
-            }
-            return  nome_registro.includes(nome) 
-        })
-     }
+    if (nome) {
+      alunasFiltradas = alunasFiltradas.filter(alunaAtual => {
+        const nome_registro = alunaAtual.nome_registro.toLowerCase()
 
-     if (cidade) {
-       alunasFiltradas = alunasFiltradas.filter(alunaAtual => {
-          return alunaAtual.cidade == cidade
-       })
-     }
+        let nome_social = alunaAtual.nome_social
 
-     if (bairro) {
-       alunasFiltradas = alunasFiltradas
-       .filter(alunaAtual => alunaAtual.bairro == bairro)
-     }
+        if (nome_social) {
+          nome_social = nome_social.toLowerCase()
+          return nome_social.includes(nome) || nome_registro.includes(nome)
+        }
+        return nome_registro.includes(nome)
+      })
+    }
 
-     if (mae) {
+    if (cidade) {
+      alunasFiltradas = alunasFiltradas.filter(alunaAtual => {
+        return alunaAtual.cidade == cidade
+      })
+    }
+
+    if (bairro) {
+      alunasFiltradas = alunasFiltradas
+        .filter(alunaAtual => alunaAtual.bairro == bairro)
+    }
+
+    if (mae) {
       alunasFiltradas = alunasFiltradas.filter(alunaAtual => {
         return alunaAtual.mae.toLowerCase().includes(mae.toLowerCase())
       })
-     }
+    }
 
-     if (pai) {
+    if (pai) {
       alunasFiltradas = alunasFiltradas.filter(alunaAtual => {
         return alunaAtual.pai.toLowerCase().includes(pai.toLowerCase())
       })
-     }
+    }
 
-     res.status(200).send(alunasFiltradas)
-   } catch (error) {
-      res.status(500).send({
-        message: error.message
-      })
-   }
+    res.status(200).send(alunasFiltradas)
+  } catch (error) {
+    res.status(500).send({
+      message: error.message
+    })
+  }
 }
 
 const obterAlunaPorId = async (req, res) => {
-   const { id } = req.params
+  const { id } = req.params
 
-   try {
-      const alunas = await db()
-      const alunaEncontrada = alunas.find(aluna => aluna.id  == id)
-      if (alunaEncontrada == undefined) {
-        return res.status(404).send({ message: "Aluna não encontrada"})
-      }
-      res.status(200).send(alunaEncontrada)
-   } catch (error) {
+  try {
+    const alunas = await db()
+    const alunaEncontrada = alunas.find(aluna => aluna.id == id)
+    if (alunaEncontrada == undefined) {
+      return res.status(404).send({ message: "Aluna não encontrada" })
+    }
+    res.status(200).send(alunaEncontrada)
+  } catch (error) {
     res.status(500).send({ message: error.message })
-   }
+  }
 }
 
 const obterNotas = async (req, res) => {
@@ -75,15 +75,15 @@ const obterNotas = async (req, res) => {
 
   try {
     const alunas = await db()
-  
+
     const alunaEncontrada = alunas
-    .find(alunaAtual => alunaAtual.id == id)
+      .find(alunaAtual => alunaAtual.id == id)
 
     const notas = alunaEncontrada.notas
-    const { 
+    const {
       ciencias_da_natureza, ciencias_humanas, linguagens_codigos,
       matematica, redacao
-     } = notas
+    } = notas
 
     const resposta = {
       ciencias_da_natureza,
@@ -102,98 +102,137 @@ const obterNotas = async (req, res) => {
   }
 }
 
-const obterBoletim = async (req, res) => {
-   // para casa
+const obterBoletins = async (req, res) => {
+  const { turma } = req.params
+  try {
+    const alunas = await db()
+    const alunasEncontradas = alunas.filter(alunaAtual => alunaAtual.turma == turma)
+    if (alunasEncontradas.length == 0) {
+      return res.status(404).json({ message: `Nenhuma aluna encontrada para a turma de ${turma}.` })
+    }
+    let notasAlunas;
+    let media;
+    let alunasFiltradas = [];
+    alunasEncontradas.forEach(aluna => {
+      notasAlunas = Object.values(aluna.notas)
+      media = (notasAlunas.reduce((acumulador, nota) => Number(acumulador) + Number(nota))) / 5
+      if (media >= 6) {
+        situacao = "APROVADA"
+      } else if (media >= 5) {
+        situacao = "RECUPERAÇÃO"
+      } else {
+        situacao = "REPROVADA"
+      }
+      const resposta = {
+        ciencias_da_natureza: aluna.notas.ciencias_da_natureza,
+        ciencias_humanas: aluna.notas.ciencias_humanas,
+        linguagens_codigos: aluna.notas.linguagens_codigos,
+        matematica: aluna.notas.matematica,
+        redacao: aluna.notas.redacao,
+        situacao: situacao,
+        media: Number(media.toFixed(2)),
+        nome: aluna.nome_social || aluna.nome_registro,
+        turma: aluna.turma
+      }
+      alunasFiltradas.push(resposta)
+    });
+
+    res.status(200).send(alunasFiltradas)
+
+  } catch (error) {
+    res.status(500).send({ message: error.message })
+  }
+
 }
 
 const criarAluna = async (req, res) => {
-   try {
-     const alunas = await db()
+  try {
+    const alunas = await db()
 
-     const alunaBody = req.body
-     const { rg, cpf, email, nome_registro } = alunaBody
-    
-     if (rg == undefined) return res.status(400).send({
+    const alunaBody = req.body
+    const { rg, cpf, email, nome_registro } = alunaBody
+
+    if (rg == undefined) return res.status(400).send({
       message: "Rg é obrigario"
-     })
-     if (cpf == undefined) return res.status(400).send({
+    })
+    if (cpf == undefined) return res.status(400).send({
       message: "Cpf é obrigatorio"
-     }) 
-     if (email == undefined || email.includes("@") == false) {
+    })
+    if (email == undefined || email.includes("@") == false) {
       return res.status(400).send({
         message: "email obrigario"
-       })
-     }
-     if (nome_registro == undefined) return res.status(400).send({
-       message: "O nome de registro é obrigario"
-     })
+      })
+    }
+    if (nome_registro == undefined) return res.status(400).send({
+      message: "O nome de registro é obrigario"
+    })
 
-     alunaBody.id = crypto.randomUUID()
+    alunaBody.id = crypto.randomUUID()
 
-     alunas.push(alunaBody)
+    alunas.push(alunaBody)
 
-     res.status(201).send(alunaBody)
-   } catch (error) {
-     res.status(500).send({
+    res.status(201).send(alunaBody)
+  } catch (error) {
+    res.status(500).send({
       message: error.message
-     })
-   }
+    })
+  }
 }
 
 const atualizarAluna = async (req, res) => {
-   const { id } = req.params
-   // 
-   const {
-   cpf, id: idDeletado, // extraimos(remover) o cpf e o id do body
+  const { id } = req.params
+  // 
+  const {
+    cpf, id: idDeletado, // extraimos(remover) o cpf e o id do body
     ...alunaBody // agrupou todo o resto, sem o id e o cpf
   } = req.body
-   // delete alunaBody.cpf; delete alunaBody.id
-   try {
-      const alunas = await db()
-      const alunaEncontrada = alunas.find(aluna => aluna.id == id)
-      
-      if (alunaEncontrada == undefined) return res.status(404).send({
-        message: "Aluna não encontrada."
-      })
+  // delete alunaBody.cpf; delete alunaBody.id
+  try {
+    const alunas = await db()
+    const alunaEncontrada = alunas.find(aluna => aluna.id == id)
 
-      const chaves = Object.keys(alunaEncontrada)
+    if (alunaEncontrada == undefined) return res.status(404).send({
+      message: "Aluna não encontrada."
+    })
 
-      if (cpf) {
-        throw new Error("O Cpf não pode ser atualizado.")
-      }
-   
-      chaves.forEach(chave => {
-        let dadoAtualizado = alunaBody[chave] // acessa a propriedade(valor) que vem body
-        let existeDado = new Boolean(dadoAtualizado) // valida se existe um dado
-        if (existeDado == true) alunaEncontrada[chave] = dadoAtualizado // atualiza o dado
-      })
+    const chaves = Object.keys(alunaEncontrada)
 
-      res.status(200).send(alunaEncontrada)
-   } catch (error) {
-     res.status(500).send({
+    if (cpf) {
+      throw new Error("O Cpf não pode ser atualizado.")
+    }
+
+    chaves.forEach(chave => {
+      let dadoAtualizado = alunaBody[chave] // acessa a propriedade(valor) que vem body
+      let existeDado = new Boolean(dadoAtualizado) // valida se existe um dado
+      if (existeDado == true) alunaEncontrada[chave] = dadoAtualizado // atualiza o dado
+    })
+
+    res.status(200).send(alunaEncontrada)
+  } catch (error) {
+    res.status(500).send({
       message: error.message
-     })
-   }
+    })
+  }
 }
 
 const deletarAluna = async (req, res) => {
-   const { id } = req.params
+  const { id } = req.params
 
-   try {
-     const alunas = await db()
-     const alunaIndice = alunas.findIndex(aluna => aluna.id == id)
-     
-     if (alunaIndice === -1) return res.status(404).send({
-       message: "Aluna não encontrada."
-     })
-    
-     alunas.splice(alunaIndice, 1)
+  try {
+    const alunas = await db()
+    const alunaIndice = alunas.findIndex(aluna => aluna.id == id)
 
-     res.status(200).send({ message: "Aluna deletada com sucesso!"})
-     
-   } catch (error) {
-      res.status(500).send({ message: error.message })
-   }
+    if (alunaIndice === -1) return res.status(404).send({
+      message: "Aluna não encontrada."
+    })
+
+    alunas.splice(alunaIndice, 1)
+
+    res.status(200).send({ message: "Aluna deletada com sucesso!" })
+
+  } catch (error) {
+    res.status(500).send({ message: error.message })
+  }
 }
 
 module.exports = {
@@ -203,5 +242,5 @@ module.exports = {
   obterTodasAsAlunas,
   obterAlunaPorId,
   obterNotas,
-  obterBoletim
+  obterBoletins
 }
